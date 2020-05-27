f6e44059f0259d138bc4bef516ce7d766370c8fc
wonglok <yellowhappy831@gmail.com>
2020-05-27 13:27:55 +0800

    github backup

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ssh -t lok@video-encoder.wonglok.com 'bash -i -c "cd video.wonglok.com; cd rendering-garden; git pull; npm i; npm run pm2restart;"'
+# ssh -t lok@video-encoder.wonglok.com 'bash -i -c "cd video.wonglok.com; cd rendering-garden; git pull; npm i; npm run pm2restart;"'
 ssh -t lok@ec2-renderer.wonglok.com 'bash -i -c "cd rendering-garden; git pull; npm i; npm run pm2restart;"'
 # ssh -t lok@ec2-renderer.wonglok.com 'bash -i -c "cd rendering-garden; ls"'
